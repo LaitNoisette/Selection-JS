ec013dc4a5a163c693b54114c48ce292da958a65
LaitNoisette <meyerfranckpro@gmail.com>
2017-04-30 12:45:16 +0200

    Selection Simple
    Selection Simple fonctionnel, travail en cours sur la selection multiple

--- js/script.js
+++ js/script.js
@@ -1,15 +1,60 @@
 console.log("début jquery");
 $(document).ready(function() {
-    console.log("ready!");
-    $('li').click(function() {
-        if ($(this).hasClass('nselect')) {
-            $(this).removeClass();
-            $(this).hadClass('select');
+    console.log("doc commence");
+    //Fonction permettant de changer la class de l'élément
+    function select_deselect(e) {
+        if (e.hasClass('nselect')) {
+            e.removeClass("nselect").addClass("select");
         } else {
-            $(this).removeClass();
-            $(this).hadClass('nselect');
+            e.removeClass("select").addClass("nselect");
+        }
+    }
+
+    //Fonction permettant de supprimer tous les éléments sélectionné
+    function del_select() {
+        $(".select").each(function() {
+            $(this).removeClass("select").addClass("nselect");
+        });
+    }
+
+    function test_ctrlkey() {
+        var ctrl = false;
+        $(document).keydown(function(e) {
+            //On verifie que la touche presse est la touche ctrl
+            //console.log("Touche selectionne");
+            if (e.which == 17) {
+                ctrl = true;
+                //console.log("touche ctrl :" + ctrl);
+            }
+
+        });
+        if (ctrl == true) {
+            return true;
+        }
+        return false;
+    }
+    //Fonction Selection/Deselection 1 éléments
+    $('li').click(function() {
+        console.log("Debut Fonction Click");
+        var click = $(this);
+        var kd = test_ctrlkey();
+
+
+        if (kd == false) {
+            console.log("Simple click");
+            del_select();
+            select_deselect(click);
         }
 
-        console.log("fonction changement class fini");
+        if (kd == true) {
+            console.log("Selection multiple")
+            select_deselect(click);
+        }
+
+
+        console.log("Valeur kd : " + kd + " fonction changement class fini Valeur click" + click);
     });
+
+
+
 });
