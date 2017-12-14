44456825609eb6d39b5ff62a8e817a91c86acf5c
LaitNoisette <meyerfranckpro@gmail.com>
2017-12-15 00:23:07 +0100

    Ajout commentaire sur le script + Rapport du projet

--- js/script.js
+++ js/script.js
@@ -2,59 +2,18 @@ console.log("début jquery");
 $(document).ready(function() {
     console.log("doc commence");
     var ctrl = false;
-    var xmouse;
-    var ymouse;
-    var mousedownbool = false;
-    var ySelectBoxInverse = false;
-    var xSelectBoxInverse = false;
-    var yPosInverseOrigin;
-    var xPosInverseOrigin;
+    var xmouse; //position de la souris en x
+    var ymouse; //postion de la souris en y 
+    var mousedownbool = false; //controle si la souris est pressé
+    var ySelectBoxInverse = false; // boolean pour savoir si la boite de selection est negative
+    var xSelectBoxInverse = false; // boolean pour savoir si la boite de selection est negative
+    var yPosInverseOrigin; //variable de stockage global de l'origine de la div 
+    var xPosInverseOrigin; //variable de stockage global de l'origine de la div
 
     //Position de chaque elements selectionnable
     $("li").each(function() {
         console.log("gauche : " + $(this).position().left + " Haut : " + $(this).position().top);
     });
-
-    //Controle de touche presse (ctrl)
-    $(document).keydown(function(e) {
-        //On verifie que la touche presse est la touche ctrl
-        console.log("Touche selectionne");
-        if (e.which == 17) {
-            ctrl = true;
-            console.log("touche ctrl :" + ctrl);
-        } else {
-            ctrl = false;
-        }
-        //ctrl = false;
-
-    });
-
-    $(document).keyup(function(e) {
-        //On verifie que la touche presse est la touche ctrl
-        console.log("Touche relache");
-        if (e.which == 17) {
-            ctrl = false;
-            console.log("touche ctrl up :" + ctrl);
-        }
-        //ctrl = false;
-
-    });
-
-    //Fonction permettant de changer la class de l'élément
-    function select_deselect(e) {
-        if (e.hasClass('nselect')) {
-            e.removeClass("nselect").addClass("select");
-        } else {
-            e.removeClass("select").addClass("nselect");
-        }
-    }
-
-    //Fonction permettant de supprimer tous les éléments sélectionné
-    function del_select() {
-        $(".select").each(function() {
-            $(this).removeClass("select").addClass("nselect");
-        });
-    }
 
     //Fonction Selection/Deselection 1 éléments
     $('li').click(function() {
@@ -77,15 +36,45 @@ $(document).ready(function() {
         console.log("Valeur ctrl : " + ctrl + " fonction changement class fini Valeur click" + click);
     });
 
-    //controle le deplacement de la souris 
-    $(document).mousemove(function(e) {
-        //console.log("x : " + e.pageX + " y : " + e.pageY);
-        xmouse = e.pageX;
-        ymouse = e.pageY;
-        if (mousedownbool == true) {
-            sizeSelectBox(xmouse, ymouse);
-            collisionSelectBox();
+    //Fonction permettant de changer la class de l'élément
+    function select_deselect(e) {
+        if (e.hasClass('nselect')) {
+            e.removeClass("nselect").addClass("select");
+        } else {
+            e.removeClass("select").addClass("nselect");
         }
+    }
+
+    //Fonction permettant de supprimer tous les éléments sélectionné
+    function del_select() {
+        $(".select").each(function() {
+            $(this).removeClass("select").addClass("nselect");
+        });
+    }
+
+    //Controle de touche presse (ctrl)
+    $(document).keydown(function(e) {
+        //On verifie que la touche presse est la touche ctrl
+        console.log("Touche selectionne");
+        if (e.which == 17) {
+            ctrl = true;
+            console.log("touche ctrl :" + ctrl);
+        } else {
+            ctrl = false;
+        }
+        //ctrl = false;
+
+    });
+
+    $(document).keyup(function(e) {
+        //On verifie que la touche relache est la touche ctrl
+        console.log("Touche relache");
+        if (e.which == 17) {
+            ctrl = false;
+            console.log("touche ctrl up :" + ctrl);
+        }
+        //ctrl = false;
+
     });
 
     //Initialisation de la boite de selection
@@ -94,15 +83,6 @@ $(document).ready(function() {
         e.preventDefault();
         initSelectBox(e.pageX, e.pageY);
         mousedownbool = true;
-    });
-
-    //Detection souris relache,remise à 0 de la box de selection
-    $(document).mouseup(function(e) {
-        console.log("mouseup");
-        mousedownbool = false;
-        $("#selectbox").css('height', 0);
-        $("#selectbox").css('width', 0);
-        $("#selectbox").hide();
     });
 
     //function permettant d'initialiser la position de la div contenant la boite de selection
@@ -115,6 +95,17 @@ $(document).ready(function() {
         $("#selectbox").show();
     }
 
+    //controle le deplacement de la souris 
+    $(document).mousemove(function(e) {
+        //console.log("x : " + e.pageX + " y : " + e.pageY);
+        xmouse = e.pageX;
+        ymouse = e.pageY;
+        if (mousedownbool == true) {
+            sizeSelectBox(xmouse, ymouse);
+            collisionSelectBox();
+        }
+    });
+
     //fonction permettant de dimensionner la box de selection
     function sizeSelectBox(xPosMouse, yPosMouse) {
         console.log("xposbox : " + $("#selectbox").css('left') + " yposbox : " + $("#selectbox").css('top'));
@@ -122,25 +113,25 @@ $(document).ready(function() {
         var xPosBox = $("#selectbox").position().left;
         var yPosBox = $("#selectbox").position().top;
 
-        //cas basique 
+        //cas basique si la position de la souris est superieur
         if (yPosInverseOrigin <= yPosMouse) {
             ySelectBoxInverse = false;
             $("#selectbox").css('top', yPosInverseOrigin);
         }
 
-        //cas basique
+        //cas basique 
         if (xPosInverseOrigin <= xPosMouse) {
             xSelectBoxInverse = false;
             $("#selectbox").css('left', xPosInverseOrigin);
         }
 
-        //cas ou la difference entre la postion de la souris et lorigine est negatif (axe y,top)
+        //cas ou la difference entre la postion de la souris et lorigine est negatif (axe y,top), condition false evite protege un double appel
         if (yPosBox > yPosMouse && ySelectBoxInverse == false) {
             ySelectBoxInverse = true;
             yPosInverseOrigin = yPosBox;
             yPosBox = yPosMouse;
-            yPosMouse = yPosInverseOrigin;
-            $("#selectbox").css('top', yPosBox);
+            yPosMouse = yPosInverseOrigin; //correspond a yPosBox
+            $("#selectbox").css('top', yPosBox); //origine de la boite en y devient la position de la souris
         }
 
         //cas ou la difference entre la position de la souris et lorigine est negatif (axe x,left)
@@ -148,20 +139,24 @@ $(document).ready(function() {
             xSelectBoxInverse = true;
             xPosInverseOrigin = xPosBox;
             xPosBox = xPosMouse;
+            xPosMouse = xPosInverseOrigin; //correspond a xPosBox
+            $("#selectbox").css('left', xPosBox); //origine de la boite en x devient la position de la souris
+        }
+
+        // la taille de la boite est negativz
+        if (xSelectBoxInverse == true) {
+            xPosBox = xPosMouse; // on alterne les positions 
             xPosMouse = xPosInverseOrigin;
             $("#selectbox").css('left', xPosBox);
         }
 
-        if (xSelectBoxInverse == true) {
-            xPosBox = xPosMouse;
-            xPosMouse = xPosInverseOrigin;
-            $("#selectbox").css('left', xPosBox);
-        }
+        //taille negative
         if (ySelectBoxInverse == true) {
             yPosBox = yPosMouse;
             yPosMouse = yPosInverseOrigin;
             $("#selectbox").css('top', yPosBox);
         }
+
         //calcul des tailles
         var xsize = Math.abs(xPosMouse - xPosBox);
         var ysize = Math.abs(yPosMouse - yPosBox);
@@ -182,13 +177,14 @@ $(document).ready(function() {
                 var xmaxE = $(this).position().left + $(this).width();
                 var ymaxE = $(this).position().top + $(this).height();
                 console.log("xminE : " + xminE + " xmaxE : " + xmaxE);
-
+                //coordonnees de la boite de selection
                 var xminSB = $("#selectbox").position().left;
                 var yminSB = $("#selectbox").position().top;
                 var xmaxSB = $("#selectbox").position().left + $("#selectbox").width();
                 var ymaxSB = $("#selectbox").position().top + $("#selectbox").height();
 
-                //gestion de la collision
+
+                //gestion de la collision (retrouver sur un cours en ligne pour la formule gerant tous les cas)
                 if ((xminSB > xmaxE) || (xmaxSB < xminE) || (yminSB > ymaxE) || (ymaxSB < yminE)) {
                     ncollision_nselect($(this));
                 } else {
@@ -213,5 +209,14 @@ $(document).ready(function() {
             e.removeClass("select").addClass("nselect");
         }
     }
+
+    //Detection souris relache,remise à 0 de la box de selection
+    $(document).mouseup(function(e) {
+        console.log("mouseup");
+        mousedownbool = false;
+        $("#selectbox").css('height', 0);
+        $("#selectbox").css('width', 0);
+        $("#selectbox").hide();
+    });
 
 });
